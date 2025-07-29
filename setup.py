076afc4085bc65f87cffc0ef2a60cfffdc14ec60
Atharv Yadav <atharvbyadav@gmail.com>
2025-07-29 14:37:39 +0530

    Updating SetUp

--- setup.py
+++ setup.py
@@ -5,14 +5,18 @@ version = {}
 with open(os.path.join("ghostpath", "version.py")) as f:
     exec(f.read(), version)
 
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
     name="GhostPath",
-    version="2.3.6",
+    version=version["__version__"],
     author="Atharv Yadav",
     description="GhostPath - Interactive Recon Shell for Ethical Hackers",
-    long_description=open("README.md").read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/atharvbyadav/GhostPath",
+    license="BSD-3-Clause",
     packages=find_packages(include=["ghostpath", "ghostpath.*"]),
     include_package_data=True,
     package_data={"ghostpath.data": ["*.txt"]},
@@ -33,4 +37,10 @@ setup(
         "License :: OSI Approved :: BSD License",
     ],
     python_requires=">=3.7",
+    keywords=["osint", "recon", "security", "hacking", "penetration-testing", "cli"],
+    project_urls={
+        "Documentation": "https://github.com/atharvbyadav/GhostPath",
+        "Source": "https://github.com/atharvbyadav/GhostPath",
+        "Tracker": "https://github.com/atharvbyadav/GhostPath/issues",
+    },
 )
